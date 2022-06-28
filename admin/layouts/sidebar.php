3169f21199f71e1050912e4389e3f6871426a816
arprince <arprince47@gmail.com>
2022-06-28 19:42:41 +0600

    added fronted for showing all books

--- admin/layouts/sidebar.php
+++ admin/layouts/sidebar.php
@@ -1,23 +1,28 @@
-    <!-- ======= Sidebar ======= -->
-    <aside id="sidebar" class="sidebar">
+<?php
+    function active($url) {
+         return ((substr($_SERVER["SCRIPT_NAME"], strrpos($_SERVER["SCRIPT_NAME"], "/") + 1)) == $url) ? "" : "collapsed";
+    }
+?>
 
+<!-- ======= Sidebar ======= -->
+    <aside id="sidebar" class="sidebar">
         <ul class="sidebar-nav" id="sidebar-nav">
 
             <li class="nav-item">
-                <a class="nav-link " href="/bookreview/dashboard.php">
+                <a class="nav-link <?php echo $cls = ((substr($_SERVER["SCRIPT_NAME"], strrpos($_SERVER["SCRIPT_NAME"], "/") + 1)) == "dashboard.php")  ? "" : "collapsed"; ?>" href="/bookreview/dashboard.php">
                     <i class="bi bi-grid"></i>
                     <span>Dashboard</span>
                 </a>
             </li><!-- End Dashboard Nav -->
             <li class="nav-heading">BOOKS</li>
             <li class="nav-item">
-                <a class="nav-link collapsed" href="/bookreview/admin/addbooks.php">
+                <a class="nav-link <?php echo $cls = ((substr($_SERVER["SCRIPT_NAME"], strrpos($_SERVER["SCRIPT_NAME"], "/") + 1)) == "addbooks.php")  ? "" : "collapsed"; ?>" href="/bookreview/admin/addbooks.php">
                     <i class="bi bi-cloud-plus"></i>
                     <span>Add Books</span>
                 </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link collapsed" href="pages-blank.html">
+                <a class="nav-link <?php echo active("books.php"); ?>" href="/bookreview/admin/books.php">
                     <i class="bi bi-gear"></i>
                     <span>Books View | Edit | Delete</span>
                 </a>
@@ -25,13 +30,13 @@
 
             <li class="nav-heading">SUMMARIES</li>
             <li class="nav-item">
-                <a class="nav-link collapsed" href="pages-blank.html">
+                <a class="nav-link <?php echo active("addsummary.php"); ?>" href="/bookreview/admin/addsummary.php">
                     <i class="bi bi-chat-square-dots"></i>
                     <span>Add Summary</span>
                 </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link collapsed" href="pages-blank.html">
+                <a class="nav-link <?php echo active("summary.php"); ?>" href="/bookreview/admin/summary.php">
                     <i class="bi bi-gear"></i>
                     <span>Summary Edit | Delete</span>
                 </a>
@@ -39,7 +44,7 @@
 
             <li class="nav-heading">PHOTOS</li>
             <li class="nav-item">
-                <a class="nav-link collapsed" href="pages-blank.html">
+                <a class="nav-link <?php echo active("media.php"); ?>" href="/bookreview/admin/media.php">
                     <!-- <i class="bi bi-gear"></i> -->
                     <i class="bi bi-images"></i>
                     <span>Photo Edit | Delete</span>
--- admin/test.php
+++ admin/test.php
@@ -3,7 +3,7 @@ require_once("init.php");
 
 // $photo = new Photo();
 
-echo Photo::find_item(1);
+// echo Photo::find_item(1);
 // $photo->file = "category.jpg";
 // $photo->book_id = 1;
 
@@ -13,6 +13,16 @@ echo Photo::find_item(1);
 //     echo "failed";
 // }
 
+// global $session;
+//  $session->login(1);
+
+
+$curPageName = substr($_SERVER["SCRIPT_NAME"], strrpos($_SERVER["SCRIPT_NAME"], "/") + 1);
+
+ print_r(explode(".", $curPageName));
+
+// echo "The current page name is: " . $curPageName;
+// echo "</br>";
 ?>
-<!-- 
-Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veritatis itaque optio excepturi eveniet pariatur animi voluptate cupiditate iste quia reiciendis. -->
+
+<!-- Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veritatis itaque optio excepturi eveniet pariatur animi voluptate cupiditate iste quia reiciendis. -->
